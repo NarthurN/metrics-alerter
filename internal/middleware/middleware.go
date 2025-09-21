59d35d1d5b0b01d139e78eed274ed205ec6fecb1
Артур Низамитдинов <anezamus-10@mail.ru>
2025-09-21 17:08:12 +0300

    add agent

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"log"
+	"math"
 	"net/http"
 	"strconv"
 
@@ -10,46 +11,42 @@ import (
 
 func ValidateMetrics(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Проверка Content-Type на text/plain
 		contentType := r.Header.Get("Content-Type")
 		if contentType != "text/plain" {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
-
 		metricType := r.PathValue("metricType")
 		metricName := r.PathValue("metricName")
 		metricValue := r.PathValue("metricValue")
 
+		// Проверка metricName на пустоту
 		if metricName == "" {
 			http.NotFound(w, r)
 			return
 		}
 
+		// Проверка metricType на валидность
 		if metricType != model.Counter && metricType != model.Gauge {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
-		if parsedValue, err := strconv.ParseFloat(metricValue, 64); err != nil {
-			log.Println("gauge не парсится")
+		// Проверка metricValue. Если gauge, то float64, если counter, то int
+		parsedValue, err := strconv.ParseFloat(metricValue, 64)
+		if err != nil {
+			log.Println("metricValue не является числом")
 			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("ошибка strconv.ParseFloat(metricValue, 64)"))
 			return
 		}
 
+		// Проверка что при counter metricValue является целым
 		if metricType == model.Counter {
-			parsedValueInt, ok := parsedValue
-		}
-
-		switch metricType {
-		case model.Gauge:
-		case model.Counter:
-
-			if _, err := strconv.ParseInt(metricValue, 10, 64); err != nil {
-				log.Println("counter не парсится")
+			if parsedValue != math.Trunc(parsedValue) {
+				log.Println("metricValue не является целым числом")
 				w.WriteHeader(http.StatusBadRequest)
-				w.Write([]byte("ошибка strconv.ParseInt(metricValue, 10, 64)"))
 				return
 			}
 		}
